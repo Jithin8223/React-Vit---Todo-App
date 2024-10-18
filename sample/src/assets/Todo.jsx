ff577f232e335886f4808fe3ac8b0204ebaaf54f
jithin8223 <jithinpbabu7020@gmail.com>
2024-10-18 18:21:19 +0530

    Work-Todo

--- sample/src/assets/Todo.jsx
+++ sample/src/assets/Todo.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import './Todo.css'
+import './Todo.css' 
+import { useState } from 'react'
 
 
 export default function Todo() {
+  const [todo, setTodo]=useState('')
   return (
     <div className='container'>
       <h1>TODO APP</h1>
       <form className='form-group'>
-        <input type="text"  placeholder='Enter Your Todo'/>
+        <input type="text" value={todo} placeholder='Enter Your Todo' className='form-control' onChange={(event)=> setInput(event.target.value)}/>
         <button>ADD</button>
       </form>
-      <div>
+      <div className='list'>
         <ul>
-            <li>First</li>
-            <li>Second</li>
-            <li>Third</li>
+            <li className='form-control'>First</li>
+            <li className='form-control'>Second</li>
+            <li className='form-control'>Third</li>
         </ul>
       </div>
     </div>
