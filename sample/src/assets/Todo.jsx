ccd0007f1940e4779e04695eaa5aa510c76ce106
jithin8223 <jithinpbabu7020@gmail.com>
2024-10-20 10:56:07 +0530

    Work1

--- sample/src/assets/Todo.jsx
+++ sample/src/assets/Todo.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
-import './Todo.css' 
-import { useState } from 'react'
-
+import React from "react";
+import "./Todo.css";
+import { useState, useRef, useEffect } from "react";
+import { IoCheckmarkDone } from "react-icons/io5";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 export default function Todo() {
-  const [todo, setTodo]=useState('')
-  const [todos,setTodos]=useState([])
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const addTodo=() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    setTodos([...todos,todo])
-    console.log(todos)
+  const addTodo = () => {
+    setTodos([...todos, {list:todo,id : Date.now()}]);
+    console.log(todos);
+    setTodo("");
+  };
 
-  }
-
+  const inputRef = useRef("null");
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+const onDelete =(id) =>{
+  setTodos(todos.filter((to)=>to.id!==id))
+}
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>TODO APP</h1>
-      <form className='form-group'>
-        <input type="text" value={todo} placeholder='Enter Your Todo' className='form-control' onChange={(event)=> setTodo(event.target.value)}/>
+      <form className="form-group" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          ref={inputRef}
+          placeholder="Enter Your Todo"
+          className="form-control"
+          onChange={(event) => setTodo(event.target.value)}
+        />
         <button onClick={addTodo}>ADD</button>
       </form>
-      <div className='list'>
+      <div className="list">
         <ul>
-            <li className='form-control'>First</li>
-            <li className='form-control'>Second</li>
-            <li className='form-control'>Third</li>
+          {todos.map((to) => (
+            <li className="list-items">
+              <div className="list-item">{to.list}</div>
+              <span>
+                <IoCheckmarkDone
+                  className="list-icons"
+                  id="complete"
+                  title="Complete"
+                />
+                <FaEdit className="list-icons" id="edit" title="Edit" />
+                <MdDelete className="list-icons" id="delete" title="Delete" onClick={()=>onDelete(to.id)}/>
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
-
